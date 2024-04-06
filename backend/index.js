28671a788ee9d7236ec81c1af1f962ac0116f8d4
Surendra Kumar <asurendrakumarpatel@gmail.com>
2024-04-06 11:06:19 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json()); 
 app.use(cookieParser());
 const corsOption={
-    origin:'https://chatapp-client-pearl.vercel.app',
+    origin:'*',
     credentials:true
 };
 app.use(cors(corsOption));
--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import toast from "react-hot-toast"
 import axios from "axios";
-import {useDispatch} from "react-redux";
+import { useDispatch } from "react-redux";
 import { setAuthUser } from '../redux/userSlice';
 import { BASE_URL } from '..';
 
@@ -23,9 +23,9 @@ const Login = () => {
         },
         withCredentials: true
       });
-       navigate("/");  
-       console.log(res);
-       dispatch(setAuthUser(res.data));
+      navigate("/");
+      console.log(res);
+      dispatch(setAuthUser(res.data));
     } catch (error) {
       toast.error(error.response.data.message);
       console.log(error);
