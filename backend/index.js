a9784e9e478cc890f7e2ba383b3c59c2969f056f
Surendra Kumar <asurendrakumarpatel@gmail.com>
2024-04-06 10:32:42 +0530

    again code changes

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json()); 
 app.use(cookieParser());
 const corsOption={
-    origin:'https://localhost:3000',
+    origin:'https://chatapp-client-pearl.vercel.app',
     credentials:true
 };
 app.use(cors(corsOption));
--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -17,7 +17,7 @@ const Login = () => {
   const onSubmitHandler = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post(` ${BASE_URL}/api/v1/user/login`, user, {
+      const res = await axios.post(`${BASE_URL}/api/v1/user/login`, user, {
         headers: {
           'Content-Type': 'application/json'
         },
